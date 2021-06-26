43fb6de32eccad751d8b53cc815f25d84b51ad9a
Syed Mohsin <syedmohsinmehmood@gmail.com>
2021-06-26 05:51:56 +0500

    Removed Defaults

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<header>
+<h2>Digital Creative Agency</h2>
+
+<ul class="menu">
+
+<li><a href="#">1</a></li>
+<li><a href="#">2</a></li>
+<li><a href="#">3</a></li>
+
+</ul>
+
+</header>
+
+</template>
+
+<style lang="scss" scoped>
+
+.menu{
+  display:flex;
+  justify-content:space-around;
+}
+
+header{
+  display:flex;
+  justify-content: space-between;
+}
+header h2{
+  flex-basis:60%;
+}
+
+
+
+
+</style>
